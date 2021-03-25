2d480005990d0943f7c9f7172ea0378274f18f67
haidy-code <haidymohamedaminsamy@gmail.com>
2021-03-25 15:17:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/heiroghliphics_translate_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.heiroghliphics_translate_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
