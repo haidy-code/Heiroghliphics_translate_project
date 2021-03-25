9cc6a20306a1b2e49c03ddc80054fcbcaf4771c7
Menna-Elberamy <mennaberam@gmail.com>
2021-03-25 22:37:51 +0200

    Bottom Navigation

--- app/src/main/java/com/example/heiroghliphics_translate_project/MainActivity.java
+++ app/src/main/java/com/example/heiroghliphics_translate_project/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.heiroghliphics_translate_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ***********************start of bottom navigation**********************
+
+//        NavController navController = Navigation.findNavController(MainActivity.this,R.id.nav_host_fragment);
+//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+//        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+//      *************************end of bottom navigation***************************
+
     }
 }
---
+++ app/src/main/java/com/example/heiroghliphics_translate_project/cameraFragment.java
@@ -0,0 +1,20 @@
+package com.example.heiroghliphics_translate_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class cameraFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false);
+    }
+}
