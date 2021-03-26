3c7db09d0c8bc97f6a0666e60ad088c27e47e333
Menna-Elberamy <mennaberam@gmail.com>
2021-03-26 04:14:05 +0200

    fragment navigation

--- app/src/main/java/com/example/heiroghliphics_translate_project/MainActivity.java
+++ app/src/main/java/com/example/heiroghliphics_translate_project/MainActivity.java
@@ -5,10 +5,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import static androidx.navigation.ui.NavigationUI.setupWithNavController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        ***********************start of bottom navigation**********************
-//
-//        NavController navController = Navigation.findNavController(MainActivity.this,R.id.nav_host_fragment);
-//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomAppBar);
-//        setupWithNavController(bottomNavigationView,navController);
 
-//        disable secand item in menu empty item
-//        bottomNavigationView.getMenu().getItem(2).setEnabled(false);
+        NavController navController = Navigation.findNavController(MainActivity.this,R.id.nav_host_fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+//        disable camera item which defined in menu as a blanked for desgin
+        bottomNavigationView.getMenu().getItem(1).setEnabled(false);
 
 
 //      *************************end of bottom navigation***************************
