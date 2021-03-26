8ffaf1a6c2b7b204dff44a8af1b1583338bfd319
Menna-Elberamy <mennaberam@gmail.com>
2021-03-26 03:25:38 +0200

    fragment change

--- app/src/main/java/com/example/heiroghliphics_translate_project/MainActivity.java
+++ app/src/main/java/com/example/heiroghliphics_translate_project/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.heiroghliphics_translate_project;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
-
-import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.ui.NavigationUI.setupWithNavController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        ***********************start of bottom navigation**********************
-
+//
 //        NavController navController = Navigation.findNavController(MainActivity.this,R.id.nav_host_fragment);
 //        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomAppBar);
-//        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+//        setupWithNavController(bottomNavigationView,navController);
 
 //        disable secand item in menu empty item
 //        bottomNavigationView.getMenu().getItem(2).setEnabled(false);
