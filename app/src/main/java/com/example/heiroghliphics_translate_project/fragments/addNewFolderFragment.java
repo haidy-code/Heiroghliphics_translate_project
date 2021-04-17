4daed6cd7643b67d2429f6794365e98bca99df26
Menna-Elberamy <mennaberam@gmail.com>
2021-04-17 02:02:58 +0200

    uploadImageFromGallery

--- app/src/main/java/com/example/heiroghliphics_translate_project/fragments/addNewFolderFragment.java
+++ app/src/main/java/com/example/heiroghliphics_translate_project/fragments/addNewFolderFragment.java
@@ -63,7 +63,6 @@ public class addNewFolderFragment extends Fragment {
 
         popUpCalender();
         backToPlacesTranslation();
-        selectIamge();
     }
 
     private void selectIamge() {
