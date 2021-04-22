1b6d63ed664156872b1b92722944f7a772685840
Menna-Elberamy <mennaberam@gmail.com>
2021-04-22 17:27:02 +0200

    Database Class and DAO

--- app/src/main/java/com/example/heiroghliphics_translate_project/room/AddFolderDAO.java
+++ app/src/main/java/com/example/heiroghliphics_translate_project/room/AddFolderDAO.java
@@ -1,7 +1,17 @@
 package com.example.heiroghliphics_translate_project.room;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface AddFolderDAO {
+//    @Query("SELECT * FROM Addnewfoldermodel")
+//    List<Addnewfoldermodel> getAllPlaces();
+
+
 }
