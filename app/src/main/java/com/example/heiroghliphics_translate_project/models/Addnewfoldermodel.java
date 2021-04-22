2b3e11891e2aaecb0d90ec7b72761e2a92898a2e
haidy-code <haidymohamedaminsamy@gmail.com>
2021-04-22 17:18:56 +0200

    trial

---
+++ app/src/main/java/com/example/heiroghliphics_translate_project/models/Addnewfoldermodel.java
@@ -0,0 +1,36 @@
+package com.example.heiroghliphics_translate_project.models;
+
+public class Addnewfoldermodel {
+    private String foldername;
+    private String date;
+    private String image;
+
+    public Addnewfoldermodel() {
+    }
+
+    public String getFoldername() {
+        return foldername;
+    }
+
+    public void setFoldername(String foldername) {
+        this.foldername = foldername;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
+
+
