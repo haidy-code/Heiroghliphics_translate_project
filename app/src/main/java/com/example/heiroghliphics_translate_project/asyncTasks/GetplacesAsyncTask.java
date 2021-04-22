feeff62a8a855e80f38ec591da0a2a93e8cb2d81
haidy-code <haidymohamedaminsamy@gmail.com>
2021-04-22 18:08:11 +0200

    assynctask

--- app/src/main/java/com/example/heiroghliphics_translate_project/asyncTasks/GetplacesAsyncTask.java
+++ app/src/main/java/com/example/heiroghliphics_translate_project/asyncTasks/GetplacesAsyncTask.java
@@ -3,12 +3,19 @@ package com.example.heiroghliphics_translate_project.asyncTasks;
 import android.os.AsyncTask;
 
 import com.example.heiroghliphics_translate_project.models.Addnewfoldermodel;
+import com.example.heiroghliphics_translate_project.room.AddFolderDAO;
 
 import java.util.List;
 
 public class GetplacesAsyncTask extends AsyncTask<Void, Void, List<Addnewfoldermodel>> {
+    private AddFolderDAO folderDAO;
+
+    public GetplacesAsyncTask(AddFolderDAO folderDAO) {
+        this.folderDAO = folderDAO;
+    }
+
     @Override
     protected List<Addnewfoldermodel> doInBackground(Void... voids) {
-        return null;
+        return folderDAO.getAllPlaces();
     }
 }
