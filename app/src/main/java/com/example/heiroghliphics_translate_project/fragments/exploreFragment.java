a47a013d0e896f4fdd88ad912886e6d8c1937df7
Menna-Elberamy <mennaberam@gmail.com>
2021-07-04 17:26:27 +0200

    symbols

--- app/src/main/java/com/example/heiroghliphics_translate_project/fragments/exploreFragment.java
+++ app/src/main/java/com/example/heiroghliphics_translate_project/fragments/exploreFragment.java
@@ -10,17 +10,43 @@ import android.view.ViewGroup;
 
 import com.example.heiroghliphics_translate_project.R;
 
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterEngineCache;
+import io.flutter.embedding.engine.dart.DartExecutor;
+
 public class exploreFragment extends Fragment {
+    public FlutterEngine flutterEngine;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cashingFlutterEngine();
 
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+    public void cashingFlutterEngine(){
+
+        // Instantiate a FlutterEngine.
+        flutterEngine = new FlutterEngine(requireContext());
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.getDartExecutor().executeDartEntrypoint(
+                DartExecutor.DartEntrypoint.createDefault()
+        );
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+                .getInstance()
+                .put("my_engine_id", flutterEngine);
+        startActivity(
+                FlutterActivity
+                        .withCachedEngine("my_engine_id")
+                        .build(requireContext())
+
+        );
+
+//        finish();
+
+
     }
 }
